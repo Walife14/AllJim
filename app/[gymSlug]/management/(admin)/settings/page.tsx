a91d91c0f95b91eeb79fcb5bb4fb5d896d2f55cd
Lucas <lferon14@gmail.com>
2026-04-04 17:22:38 +0100

    add logout button to the settings in management settings

--- app/[gymSlug]/management/(admin)/settings/page.tsx
+++ app/[gymSlug]/management/(admin)/settings/page.tsx
@@ -1,7 +1,18 @@
+import SignOutButton from "@/app/components/auth/signOutButton"
+
 type Props = {}
 
-export default function Settings({}: Props) {
+export default function Settings({ }: Props) {
   return (
-    <div>Settings Page</div>
+    <div>
+      <h1>Settings</h1>
+
+      <div className="flex flex-col items-start">
+        <p>To log out of your account, click the button below:</p>
+
+        <SignOutButton />
+      </div>
+
+    </div>
   )
 }
