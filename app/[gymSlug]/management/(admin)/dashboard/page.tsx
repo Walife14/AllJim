9d8b8fec509d7badab6478acdb1ee0b6a69e3ce3
Lucas <lferon14@gmail.com>
2026-04-04 17:57:37 +0100

    add active staff component for management dashboard

--- app/[gymSlug]/management/(admin)/dashboard/page.tsx
+++ app/[gymSlug]/management/(admin)/dashboard/page.tsx
@@ -1,12 +1,18 @@
 import AttendanceStats from "@/app/components/management/AttendanceStats"
+import StaffOverview from "@/app/components/management/StaffOverview"
 
 type Props = {}
 
-export default function Dashboard({}: Props) {
+export default function Dashboard({ }: Props) {
   return (
     <div>
-        <h1>Dashboard</h1>
+      <h1>Dashboard</h1>
+
+      <div className="flex gap-4">
         <AttendanceStats daily={50} weekly={200} monthly={850} />
+        <StaffOverview />
+      </div>
+
     </div>
   )
 }
--- app/components/management/AttendanceStats.tsx
+++ app/components/management/AttendanceStats.tsx
@@ -6,21 +6,27 @@ type Props = {
 
 export default function AttendanceStats({ daily, weekly, monthly }: Props) {
   return (
-    <div className="bg-neutral-200 rounded-lg p-4 grid grid-cols-2 gap-2">
-      <div className="col-span-2 flex justify-center">
-        <div className="flex flex-col items-center bg-neutral-50 p-4 rounded-lg">
-          <span className="text-xl font-bold">{daily}</span>
-          <span>Visitor&apos;s today</span>
+    <section>
+      <div className="bg-neutral-200 p-4 rounded-lg">
+        <h2 className="text-center">Visitors</h2>
+        <div className="grid grid-cols-2 gap-2">
+
+          <div className="col-span-2 flex justify-center">
+            <div className="flex flex-col items-center bg-neutral-50 p-4 rounded-lg">
+              <span className="text-xl font-bold">{daily}</span>
+              <span>Visitor&apos;s today</span>
+            </div>
+          </div>
+          <div className="flex flex-col items-center bg-neutral-50 p-4 rounded-lg row-start-2">
+            <span className="text-xl font-bold">{weekly}</span>
+            <span>Visitor&apos;s this week</span>
+          </div>
+          <div className="flex flex-col items-center bg-neutral-50 p-4 rounded-lg row-start-2">
+            <span className="text-xl font-bold">{monthly}</span>
+            <span>Visitor&apos;s this month</span>
+          </div>
         </div>
       </div>
-      <div className="flex flex-col items-center bg-neutral-50 p-4 rounded-lg row-start-2">
-        <span className="text-xl font-bold">{weekly}</span>
-        <span>Visitor&apos;s this week</span>
-      </div>
-      <div className="flex flex-col items-center bg-neutral-50 p-4 rounded-lg row-start-2">
-        <span className="text-xl font-bold">{monthly}</span>
-        <span>Visitor&apos;s this month</span>
-      </div>
-    </div>
+    </section>
   )
 }
