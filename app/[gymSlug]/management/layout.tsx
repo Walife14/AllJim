ae03b41d04435185467aad957ff58d2842a8c3a4
Lucas <lferon14@gmail.com>
2026-04-05 20:27:56 +0100

    remove management auth folder add all files under (admin) to management route

---
+++ app/[gymSlug]/management/layout.tsx
@@ -0,0 +1,52 @@
+import { redirect } from "next/navigation";
+
+// components
+import ManagementNavbar from "@/app/components/management/ManagementNavbar";
+import { createClient } from "@/lib/supabase/server";
+
+type Props = {
+    children: React.ReactNode;
+    params: Promise<{ gymSlug: string }>
+}
+export default async function ManagementLayout({ children, params }: Props) {
+    const { gymSlug: slug } = await params
+
+    const supabase = await createClient()
+
+    // identity
+    const { data: { user } } = await supabase.auth.getUser()
+
+    // if there is no user maybe they're the owner and send to login of management
+    if (!user) {
+        redirect(`/${slug}/login`)
+    }
+
+    // we have a user check if they have a membership and what their role is
+    const { data: membership, error } = await supabase
+        .from('memberships')
+        .select(`
+            role,
+            gyms!inner (
+                slug
+            )
+        `)
+        .eq('user_id', user.id)
+        .eq('gyms.slug', slug)
+        .maybeSingle()
+
+    // handle redirection based on role/ownership
+    if (error || !membership) {
+        return redirect(`/${slug}/member/join`)
+    }
+
+    if (membership.role === 'member') {
+        return redirect(`/${slug}/member/portal`)
+    }
+
+    return (
+        <>
+            <ManagementNavbar />
+            {children}
+        </>
+    )
+}
--- app/[gymSlug]/management/page.tsx
+++ app/[gymSlug]/management/page.tsx
@@ -1,52 +1,19 @@
-import { createClient } from "@/lib/supabase/server"
-import { redirect } from "next/navigation"
 
-type Props = {
-    params: Promise<{
-        gymSlug: string
-    }>
-}
+// components
+import AttendanceStats from "@/app/components/management/AttendanceStats"
+import StaffOverview from "@/app/components/management/StaffOverview"
 
-export default async function ManagementRootPage({ params }: Props) {
-    const { gymSlug: slug } = await params
+type Props = {}
 
-    const supabase = await createClient()
+export default function ManagementPage({ }: Props) {
+    return (
+        <>
+            <h1>Dashboard</h1>
 
-    // identity
-    const { data: { user } } = await supabase.auth.getUser()
-
-    // if there is no user maybe they're the owner and send to login of management
-    if (!user) {
-        redirect(`/${slug}/management/login`)
-    }
-
-    // we have a user check if they have a membership and what their role is
-    const { data: membership, error } = await supabase
-        .from('memberships')
-        .select(`
-            role,
-            gyms!inner (
-                slug
-            )
-        `)
-        .eq('user_id', user.id)
-        .eq('gyms.slug', slug)
-        .maybeSingle()
-
-    // if they arent a member of this gym redirect them out of here
-    if (error || !membership) {
-        return redirect(`/${slug}/member/join`)
-    }
-
-    // they are a member of this gym check role and route
-    const role = membership.role
-
-    if (role === 'owner') {
-        return redirect(`/${slug}/management/dashboard`)
-    }
-
-    if (role === 'member') {
-        return redirect(`/${slug}/member/portal`)
-    }
-
+            <div className="flex gap-4">
+                <AttendanceStats daily={50} weekly={200} monthly={850} />
+                <StaffOverview />
+            </div>
+        </>
+    )
 }
