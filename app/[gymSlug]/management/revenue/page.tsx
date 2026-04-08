42368ca882d9a76d4bcd73b00a9bfe5745880e1d
Lucas <lferon14@gmail.com>
2026-04-08 11:44:55 +0100

    feat(management-revenue): add ui skeleton

--- app/[gymSlug]/management/revenue/page.tsx
+++ app/[gymSlug]/management/revenue/page.tsx
@@ -1,7 +1,56 @@
 type Props = {}
 
-export default function Revenue({}: Props) {
+export default function Revenue({ }: Props) {
   return (
-    <div>Revenue Page</div>
+    <div>
+      <h1>Revenue</h1>
+
+      {/* filter component to toggle from graphical to compact text view */}
+
+      <section>
+        <h2>Monthly Revenue Overview &#40;April 2026&#41;</h2>
+
+        <ul className="flex flex-col gap-y-3 divide-y-2">
+          <li>
+            Total Collected: <span className="font-mono">&pound;2,450.00</span>
+          </li>
+          <li>
+            Expected Total: <span className="font-mono">&pound;3,100.00</span>
+          </li>
+          <li>
+            Collection Progress: <span className="font-mono">79%</span>
+          </li>
+          <li>
+            Outstanding/Failed: <span className="font-mono">&pound;650.00</span> &#40;12 members&#41;
+          </li>
+        </ul>
+      </section>
+
+      <section>
+        <h2>Membership Breakdown</h2>
+
+        <ul className="flex flex-col gap-y-3 divide-y-2">
+          <li>
+            Tier 1 &#40;&pound;2/mo&#41;: 184 Members &mdash; <span className="font-mono">&pound;368.00</span>
+          </li>
+          <li>
+            Tier 2 &#40;&pound;10/mo&#41;: 45 Members &mdash; <span className="font-mono">&pound;450.00</span>
+          </li>
+        </ul>
+      </section>
+
+      <section>
+        <h2>Till Revenue</h2>
+
+        <ul className="flex flex-col gap-y-3 divide-y-2">
+          <li>
+            Water &#40;500ml&#41;: 112 sold &mdash; <span className="font-mono">&pound;168.00</span>
+          </li>
+          <li>
+            Protein Shakes: 45 sold &mdash; <span className="font-mono">&pound;135.00</span>
+          </li>
+        </ul>
+      </section>
+    </div>
   )
 }
