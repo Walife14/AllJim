759833597c75de1aca523ace88ebcb2d08f5ba2d
Lucas <lferon14@gmail.com>
2026-05-04 17:11:39 +0100

    feat(kisok): terminal home now  displays count of check-ins for the day

--- app/[gymSlug]/kiosk/terminal/page.tsx
+++ app/[gymSlug]/kiosk/terminal/page.tsx
@@ -1,19 +1,41 @@
-type Props = {}
+import { createClient } from "@/lib/supabase/server"
+import { endOfDay, startOfDay } from "date-fns"
 
-export default function TerminalPage({}: Props) {
+type Props = {
+  params: Promise<{
+    gymSlug: string
+  }>
+}
+
+const today = new Date()
+const start = startOfDay(today).toISOString()
+const end = endOfDay(today).toISOString()
+
+export default async function TerminalPage({ params }: Props) {
+  const { gymSlug } = await params
+  const supabase = await createClient()
+
+  // grab the gym id
+  const { data: gymData, error: gymError } = await supabase
+    .from('gyms').select('id').eq('slug', gymSlug).single()
+
+  if (gymError) return (<p>{gymError.message}</p>)
+
+  const { count, error: checkInCountError } = await supabase
+    .from('check_ins')
+    .select('', { count: 'exact', head: true })
+    .match({ gym_id: gymData.id })
+    .gte('created_at', start)
+    .lte('created_at', end)
+
+  if (checkInCountError) return (<p>{checkInCountError.message}</p>)
+
   return (
-    <div>
-        {/* Navigation */}
-        <ul>
-            <li>Current user: User Name</li>
-            <li>Home</li>
-            <li>Transaction History</li>
-            <li>Settings</li>
-            <li>Log out</li>
-        </ul>
-        <div>
-            Your terminal home page
-        </div>
+    <div className="flex flex-col items-start">
+      <div className="bg-neutral-200 p-2 rounded-lg text-center">
+        <h2>Check-ins today</h2>
+        <span className="text-3xl font-semibold">{count}</span>
+      </div>
     </div>
   )
 }
