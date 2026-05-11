26332e7d385f230320ecb6376c422616d6f77281
Lucas <lferon14@gmail.com>
2026-05-11 15:52:30 +0100

    feat(management-members): add desktop styling to contact in view member

--- app/[gymSlug]/management/members/[id]/_components/Contact.tsx
+++ app/[gymSlug]/management/members/[id]/_components/Contact.tsx
@@ -8,26 +8,23 @@ export default function Contact({ email, phone }: Props) {
         <div className="bg-neutral-200 rounded-lg p-4">
             <h2 className="text-center">Contact Details</h2>
             <div className="grid grid-cols-2 gap-2">
-                <div className="grid grid-cols-2 gap-2">
+                <div className="flex flex-col gap-2">
                     <div className="col-span-2 text-center bg-white rounded-lg p-2">
-                        <p>Email address</p>
-                        <span>{email}</span>
+                        <dt className="text-sm">Email address</dt>
+                        <dd className="font-bold">{email}</dd>
                     </div>
-                    <div className="bg-neutral-600 text-white p-2 text-center rounded-lg">
-                        <p>Copy</p>
-                    </div>
-                    <div className="bg-neutral-600 text-white p-2 text-center rounded-lg">
-                        <p>Send email</p>
+                    <div className="grid grid-cols-2 gap-2">
+                        <button className="open-btn" disabled>Copy</button>
+                        <button className="open-btn" disabled>Send Email</button>
                     </div>
                 </div>
                 <div className="grid grid-cols-2 gap-2">
                     <div className="col-span-2 text-center bg-white rounded-lg p-2">
-                        <p>Phone number</p>
-                        <span>{phone ? phone : 'No phone number added'}</span>
+                        <dt className="text-sm">Phone number</dt>
+                        <dd className="font-bold">{phone ? phone : 'No phone number added'}</dd>
                     </div>
-                    <div className="col-span-2 bg-neutral-600 text-white p-2 text-center rounded-lg">
-                        <p>Call</p>
-                    </div>
+                    <button className="open-btn" disabled>Copy</button>
+                    <button className="open-btn" disabled>Call</button>
                 </div>
             </div>
         </div>
