6e864ebcd3ecd1d0524478379396b42492134797
Lucas <lferon14@gmail.com>
2026-05-12 15:08:25 +0100

    feat(management-settings): add desktop ui

--- app/[gymSlug]/management/settings/page.tsx
+++ app/[gymSlug]/management/settings/page.tsx
@@ -4,14 +4,20 @@ type Props = {}
 
 export default function Settings({ }: Props) {
   return (
-    <div>
-      <h1>Settings</h1>
-
-      <div className="flex flex-col items-start">
-        <p>To log out of your account, click the button below:</p>
-
-        <SignOutButton mode="light" />
+    <div className="flex flex-col gap-2">
+      <div className="bg-zinc-200 rounded-lg p-4">
+        <h1>Settings</h1>
       </div>
+
+      <section className="bg-zinc-200 rounded-lg p-4">
+        <h2>FAQ</h2>
+        <p className="text-red-500">TO BE IMPLEMENTED</p>
+      </section>
+
+      <section className="p-4 flex flex-col gap-2 bg-zinc-200 rounded-lg">
+        <p className="text-sm">To sign out of your account, click the button below.</p>
+        <SignOutButton mode="dark" />
+      </section>
 
     </div>
   )
